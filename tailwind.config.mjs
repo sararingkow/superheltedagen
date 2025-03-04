1b1261c59e0a7ee372e0d1de6dfae505e2c86515
Sara <sape0003@stud.kea.dk>
2025-03-04 14:22:48 +0100

    Opstart på kode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,144 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      rose: {
+        50: "#fef1f8",
+        100: "#fee5f3",
+        200: "#ffcbe9",
+        300: "#ffa0d5",
+        400: "#ff66b7",
+        500: "#fc4fa4",
+        600: "#ec1674",
+        700: "#ce0859",
+        800: "#aa0a4b",
+        900: "#8d0e41",
+        950: "#570023",
+      },
+
+      yellow: {
+        50: "#fffee7",
+        100: "#fffec1",
+        200: "#fff886",
+        300: "#ffec41",
+        400: "#ffdb0d",
+        500: "#ffcc00",
+        600: "#d19500",
+        700: "#a66a02",
+        800: "#89530a",
+        900: "#74430f",
+        950: "#442304",
+      },
+      green: {
+        50: "#f4faf3",
+        100: "#e5f5e3",
+        200: "#cce9c9",
+        300: "#a3d79e",
+        400: "#73bc6c",
+        500: "#57b04f",
+        600: "#3c8336",
+        700: "#33672e",
+        800: "#2b5328",
+        900: "#254423",
+        950: "#0e250e",
+      },
+      turquoise: {
+        50: "#f1fafa",
+        100: "#dbf1f2",
+        200: "#bce3e5",
+        300: "#90cfd4",
+        400: "#57b0b9",
+        500: "#3c949e",
+        600: "#347a86",
+        700: "#30646e",
+        800: "#2e535c",
+        900: "#2a474f",
+        950: "#182d34",
+      },
+      gray: {
+        50: "#f6f6f6",
+        100: "#e7e7e7",
+        200: "#d1d1d1",
+        300: "#b0b0b0",
+        400: "#888888",
+        500: "#6d6d6d",
+        600: "#5d5d5d",
+        700: "#4f4f4f",
+        800: "#454545",
+        900: "#3d3d3d",
+        950: "#292929",
+      },
+    },
+
+    fontFamily: {
+      "satoshi-regular": ["Satoshi-regular", "sans-serif"],
+      "satoshi-bold": ["Satoshi-bold", "sans-serif"],
+      "satoshi-black": ["Satoshi-black", "sans-serif"],
+      "satoshi-medium": ["Satoshi-medium", "sans-serif"],
+      "satoshi-italic": ["Satoshi-italic", "sans-serif"],
+    },
+    fontSize: {
+      "display-desktop": ["6.5rem", { lineHeight: "5.5rem" }],
+      "display-mobile": ["3.9rem", { lineHeight: "3.5rem" }],
+      "h1-desktop": ["4.8rem", { lineHeight: "3.75rem" }],
+      "h1-mobile": ["3.5rem", { lineHeight: "3.75rem" }],
+      "h2-desktop": ["4rem", { lineHeight: "3.75rem" }],
+      "h2-mobile": ["2.5rem", { lineHeight: "2.5rem" }],
+      "h3-desktop": ["2.3rem", { lineHeight: "2.5rem" }],
+      "h3-mobile": ["2rem", { lineHeight: "2.25rem" }],
+      "h4-desktop": ["1.8rem", { lineHeight: "2.25rem" }],
+      "h4-mobile": ["1.4rem", { lineHeight: "1.875rem" }],
+      "p-desktop": ["1.125rem", { lineHeight: "1.875rem" }],
+      "p-mobile": ["1rem", { lineHeight: "1.5rem" }],
+      "a-desktop": ["1.125rem", { lineHeight: "1.875rem" }],
+      "a-mobile": ["1rem", { lineHeight: "1.5rem" }],
+    },
+    fontWeight: {
+      light: "300",
+      regular: "400",
+      semibold: "600",
+      bold: "700",
+      black: "900",
+    },
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0px",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+    },
+    borderRadius: {
+      DEFAULT: "1.25rem",
+      none: "0px",
+      s: "5px",
+      sm: "10px",
+      m: "15px",
+      md: "30px",
+      lg: "40px",
+      full: "9999px",
+    },
+    boxShadow: {
+      drop25: "4px 4px 4px 0 rgb(0 0 0 / 0.25)",
+      drop50: "4px 4px 4px 0 rgb(0 0 0 / 0.50)",
+      inner25: "0 4px 4px 0 rgb(0 0 0 / 0.25) inset",
+      inner50: "0 4px 4px 0 rgb(0 0 0 / 0.50) inset",
+    },
+
+    extend: {
+      spacing: {
+        gap: "40px", // Keeps the 30px gap
+        "mobil-bottom": "100px", // Slightly reduced bottom spacing for mobile
+        "desktop-bottom": "120px", // Reduced bottom spacing for desktop
+        "mobil-top": "100px", // Reduced top spacing for mobile
+        "desktop-top": "120px", // Reduced top spacing for desktop
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
